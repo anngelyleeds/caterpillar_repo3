666bb6e1a1706aefedd243cb4118eb04d4897b36
anngelyleeds <acl154@georgetown.edu>
2023-10-27 09:31:04 -0400

    update development_time 2

--- caterpillar/R/development_function.R
+++ caterpillar/R/development_function.R
@@ -8,13 +8,17 @@
 #' @export
 
 development_time <- function(day1, day2, day3, day4){
-  if (missing(day4)){
-    d_time <- (difftime(day3, day1, unit="days"))/3
-  } else if ((missing(day3)) && (missing(day4))){
-    d_time <- (difftime(day2, day1, unit="days"))/2
-  } else if ((missing(day2)) && (missing(day3)) && (missing(day(4)))){
+
+   if ((missing(day2)) && (missing(day3)) && (missing(day(4)))){
     d_time <- 0
-  } else {
+
+    } else if ((missing(day3)) && (missing(day4))){
+      d_time <- (difftime(day2, day1, unit="days"))/2
+
+    } else if (missing(day4)){
+      d_time <- (difftime(day3, day1, unit="days"))/3
+
+    } else {
     d_time <- (difftime(day4, day1, unit="days"))/4
   }
   return(d_time)
